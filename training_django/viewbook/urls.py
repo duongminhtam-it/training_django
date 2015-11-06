85d68d37dc8be24d2494657d6d159227ff0f4aa7
daniel0905 <duongminhtam.it@gmail.com>
2015-11-06 13:25:50 +0700

    delete cmt

--- training_django/viewbook/urls.py
+++ training_django/viewbook/urls.py
@@ -2,6 +2,5 @@ from django.conf.urls import url
 from viewbook import views
 
 urlpatterns = [
-    # url(r'^(?P<pk>\d+)', views.ViewListBook.as_view(), name="viewlistbook"),
     url(r'^', views.ViewListBook.as_view(), name="viewlistbook1")
 ]
--- training_django/viewbook/views.py
+++ training_django/viewbook/views.py
@@ -8,11 +8,6 @@ from django.core.paginator import Paginator
 class ViewListBook(ListView):
     model = Book
     paginate_by = 2
-    # queryset = Book.objects.all()
-
-    # def get_request(self, request):
-    #     key_search = request.GET.get('q', '')
-    #     return self.get_queryset()
 
     def get_queryset(self):  # Get data from database and setting, this is stronger get_context_data
         if self.request.GET.get('q'):
@@ -20,7 +15,3 @@ class ViewListBook(ListView):
             return Book.objects.filter(name__icontains=key_search)
         else:
             return Book.objects.all()
-
-    # def get_context_data(self, **kwargs): # Get all data from database
-    #     context = super(ViewListBook, self).get_context_data(**kwargs)
-    #     return context
